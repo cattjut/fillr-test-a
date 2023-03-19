3123a9df42cffab2dfe1a6c96f86009d976b96ac
e_tiutiunnikova <ekaterina@apptegral.com>
2023-03-19 08:20:01 +0000

    Add extract metadata function

--- extract.js
+++ extract.js
@@ -1,5 +1,17 @@
-module.exports.extract = function(window) {
-  
-  // Write your solution to Task #2 - Extract Metadata task here
-
+module.exports.extract = function (window) {
+  const dataFormNumber = 1;
+  var metadata = {};
+  for (i = 0; i < window.document.forms[dataFormNumber].elements.length; i++) {
+    const element = window.document.forms[dataFormNumber].elements[i];
+    if (element.name) {
+      // remove all wrapping spaces
+      var text = element.parentNode.parentNode.textContent.trim();
+      // For country we need to select the string before \n because it contains all select options
+      if (element.name == 'country') {
+        text = element.parentNode.parentNode.textContent.split('\n')[0].trim();
+      }
+      metadata[text] = text + " " + element.name;
+    }
+  }
+  return metadata
 }
