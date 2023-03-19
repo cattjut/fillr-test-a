7a4848d366eb7c80a43c23c31b0b297f8a9f56be
e_tiutiunnikova <ekaterina@apptegral.com>
2023-03-19 08:41:22 +0000

    Add match function to match credit card date fields using regex

--- match.js
+++ match.js
@@ -1,5 +1,11 @@
-module.exports.match = function(hash) {
-
-  // Write your solution to Task #2 - Match Metadata task here
-  
-}
+module.exports.match = function (hash) {
+  // Assuming that the 'credit card date fields' contain 'card' and 'date' strings
+  const regex = /^card.+date.+/i;
+  var result = [];
+  for (var key in hash) {
+    if (regex.test(key)) {
+      result.push(key);
+    }
+  }
+  return result;
+};
