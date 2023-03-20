530db944b3fcdda7d20ccb338cbcbd6ce637dcbd
e_tiutiunnikova <ekaterina@apptegral.com>
2023-03-20 10:18:57 +0000

    Added a function to create a mapping,json (Task 1)

--- form_mapping.js
+++ form_mapping.js
@@ -12,9 +12,86 @@
 // Save the complete JSON hash (formatted and indented) to the file named mapping.json
 // You can implement a script that outputs mapping.json, or you can write out the output manually.
 
+function returnKeyByRegexMatch(fieldName, dict, defaultReturn) {
+    for (var key in dict) {
+        if (dict[key].test(fieldName)) {
+            return key
+        }
+    }
+    return defaultReturn
+}
+
+function adressType(fieldName) {
+    // Dict of address types : regex
+    var addressTypes = {
+        'SHIPPING': /.*shipto.*/i,
+        'BILLING': /.*billto.*/i,
+        'RECEIPT': /.*receiptto.*/i
+    }
+    return returnKeyByRegexMatch(fieldName, addressTypes, 'HOME')
+}
+
+function sectionType(fieldName) {
+    // Dict of section names : regex
+    var sectionNames = {
+        'card information': /.*card.*/i,
+        'address': /.*addr|postalcode|city|zip|country|state|street.*/i,
+        'name': /.*name.*/i,
+        'phone': /.*phone.*/i,
+        'email': /.*email.*/i,
+        'username': /.*username.*/i,
+        'password': /.*password.*/i,
+        'organization': /.*organization|company.*/i
+    }
+    return returnKeyByRegexMatch(fieldName, sectionNames, null)
+}
+
+function autofillFieldName(fieldName) {
+    // Dict of autofill field names : regex
+    var autofillFieldNames = {
+        'cc-exp-month': /.*card_expdate_month.*/i,
+        'cc-exp-year': /.*card_expdate_year.*/i,
+        'cc-exp': /.*card_expdate_day.*/i,
+        'cc-number': /.*card_number.*/i,
+        'cc-csc': /.*card_verification.*/i,
+        'cc-type': /.*card_type.*/i,
+        'cc-name': /.*card_name.*/i,
+        'address-line1': /.*addr|city|zip|state|street.*/i,
+        'honorific-prefix': /.*name_prefix.*/i,
+        'honorific-suffix': /.*name_suffix.*/i,
+        'given-name': /.*name_first.*/i,
+        'additional-name': /.*name_middle.*/i,
+        'family-name': /.*name_last.*/i,
+        'country': /.*country.*/i,
+        'postal-code': /.*postalcode.*/i,
+        'tel': /.*phone.*/i,
+        'email': /.*email.*/i,
+        'username': /.*username.*/i,
+        'password': /.*password.*/i,
+        'organization': /.*organization|company.*/i
+    }
+
+    return returnKeyByRegexMatch(fieldName, autofillFieldNames, null)
+}
 
 function formMapping(window) {
-    return 5
+    var mapping = {}
+    for (i = 0; i < window.document.forms[1].elements.length; i++) {
+        const element = window.document.forms[1].elements[i];
+        var sectionTypeStr = sectionType(element.name)
+        var adressTypeStr = null
+        if (sectionTypeStr == 'address') {
+            adressTypeStr = adressType(element.name)
+        }
+        var autofillFieldNameStr = autofillFieldName(element.name)
+        mapping[element.name] = {
+            'Section Type': sectionTypeStr,
+            'Adress Type': adressTypeStr,
+            'Autofill Field Name': autofillFieldNameStr
+        }
+    }
+
+    return mapping
 }
 
 module.exports = { formMapping };
--- run_form_mapping.js
+++ run_form_mapping.js
@@ -2,16 +2,16 @@ var jsdom = require("jsdom/lib/old-api");
 const fs = require('fs');
 const { formMapping } = require('./form_mapping');
 const url = "assets/autofill.mozdev.org.autofilltest.html"
+const html = fs.readFileSync(url, 'utf8')
 
 
 jsdom.env({
-    html: url,
+    html: html,
     scripts: [],
     done: (err, window) => {
         var result = formMapping(window);
-        console.log('First result', result)
         // Create a json file from result data
-        fs.writeFile('mapping.json', JSON.stringify(result), (err) => {
+        fs.writeFile('mapping.json', JSON.stringify(result, null, 2), (err) => {
             if (err) {
                 console.error(err);
                 return;
